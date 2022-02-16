c3eadf6b0de0918d5d2b26cb73b4fddd75874a9d
Rafael Pereira Souto Da Silva <rafaelsccp0906@gmail.com>
2022-02-15 22:01:01 -0300

    att total ( design e funcional )

--- script.js
+++ script.js
@@ -1,102 +1,29 @@
-// Digitos
-const btn1 = document.querySelector('#n1')
-const btn2 = document.querySelector('#n2')
-const btn3 = document.querySelector('#n3')
-const btn4 = document.querySelector('#n4')
-const btn5 = document.querySelector('#n5')
-const btn6 = document.querySelector('#n6')
-const btn7 = document.querySelector('#n7')
-const btn8 = document.querySelector('#n8')
-const btn9 = document.querySelector('#n9')
-
-// Operadores 
-const somar = document.querySelector('#somar')
-const subtrair = document.querySelector('#subtrair')
-const multiplicar = document.querySelector('#multiplicar')
-const dividir = document.querySelector('#dividir')
-
-const visor = document.querySelector('#visor')
-
-var nX = ''
-var nY = ''
-var operador = '' 
+const resultado = document.querySelector('#resultado')
 
 
-btn1.addEventListener('click', function() {
-    const n = 1
-    adicionarX(n)
-})
+function estilo() {
+    let nume = document.querySelectorAll('.nume')
+    nume.classList.add('.red')
+}
 
-btn2.addEventListener('click', function() {
-    const n = 2
-    adicionarX(n)
-})
+function inserir(num) {
+    resultado.innerHTML += num
+}
 
-btn3.addEventListener('click', function() {
-    const n = 3
-    adicionarX(n)
-})
+function limpar() {
+    document.getElementById('resultado').innerHTML = ''
+}
 
-btn4.addEventListener('click', function() {
-    const n = 4
-    adicionarX(n)
-})
+function del() {
+    let resultado = document.getElementById('resultado').innerHTML
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1)
+}
 
-btn5.addEventListener('click', function() {
-    const n = 5
-    adicionarX(n)
-})
-
-btn6.addEventListener('click', function() {
-    const n = 6
-    adicionarX(n)
-})
-
-btn7.addEventListener('click', function() {
-    const n = 7
-    adicionarX(n)
-})
-
-btn8.addEventListener('click', function() {
-    const n = 8
-    adicionarX(n)
-})
-
-btn9.addEventListener('click', function() {
-    const n = 9
-    adicionarX(n)
-})
-
-somar.addEventListener('click', function() {
-    operador = '+'
-    addVisor
-})
-
-
-
-function adicionarX(n) {
-    if (operador.length === 0) {
-        nX = nX.concat(n)
-        console.log(nX)
-        addVisor()
-    } 
-
-    if (operador.length > 0) {
-        nY = nY.concat(n)
-        console.log(nY)
-        addVisor()
+function calcular() {
+    let resultado = document.getElementById('resultado').innerHTML;
+    if(resultado) {
+        document.getElementById('resultado').innerHTML = eval(resultado)
+    } else {
+        document.getElementById('resultado').innerHTML = 'erro'
     }
 }
-
-function addVisor() {
-    visor.innerHTML = x
-}
-
-
-var a = '11'
-var b = '+'
-var c = '2'
-
-var d = parseInt(a.concat(b).concat(c))
-
-
